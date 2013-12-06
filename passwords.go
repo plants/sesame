4ded1f8dc98aeda430345b011a8babf4582a7031
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-12-06 07:01:26 -0600

    recover from rebase accident
    This was 5 commits, I erased them all with rebase. Oops.
    
    This commit now changes the password hashing mechanism to scrypt and
    adds a user struct. It also ignores .test files in .gitignore

--- passwords.go
+++ passwords.go
@@ -1,8 +1,13 @@
 package sesame
 
-import "code.google.com/p/go.crypto/bcrypt"
+import "code.google.com/p/go.crypto/scrypt"
 
-const Cost = 10
+const (
+	scrypt_N      = 16384
+	scrypt_r      = 8
+	scrypt_p      = 1
+	scrypt_keyLen = 64
+)
 
 type Password []byte
 
@@ -12,8 +17,8 @@ func clear(b []byte) {
 	}
 }
 
-func NewPassword(s Salt, plaintext []byte) (password Password, err error) {
-	password, err = bcrypt.GenerateFromPassword(append(s, plaintext...), Cost)
+func NewPassword(salt Salt, plaintext []byte) (password Password, err error) {
+	password, err = scrypt.Key(plaintext, salt, scrypt_N, scrypt_r, scrypt_p, scrypt_keyLen)
 
 	defer clear(plaintext)
 	return
